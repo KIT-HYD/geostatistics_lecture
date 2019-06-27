96b7a8d7a9afb714c63505d69b99e44859a32424
Mirko Mälicke <mirko.maelicke@kit.edu>
2019-06-27 12:49:32 +0200

    Update test_cressie.m

--- variogram/test_cressie.m
+++ variogram/test_cressie.m
@@ -9,7 +9,7 @@ x2 = [4 8 7 3 0 12 9 8 7 6 5 4 33 3 8 3 8 6 7 5 6 8];
 x3 = [-4 5 -7 18 -5  -1 0 0 2 3 -18 -2 -5 8 7 6 5];
 
 % test x1:
-if abs(cressie(x1) - 2.03) > sig
+if abs(cressie(x1) - 1.015) > sig
   error('Test 1 failed!');
 else
   disp('Test 1 successful.');
@@ -17,7 +17,7 @@ end
 
 
 % test x2:
-if abs(cressie(x2) - 83.29) > sig
+if abs(cressie(x2) - 41.645) > sig
   error('Test 2 failed!');
 else
   disp('Test 2 successful.');
@@ -25,7 +25,7 @@ end
 
 
 % test x3:
-if abs(cressie(x3) - 39.39) > sig
+if abs(cressie(x3) - 19.695) > sig
   error('Test 3 failed!');
 else
   disp('Test 3 successful.');
