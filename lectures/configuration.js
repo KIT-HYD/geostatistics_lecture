f5830cbfd0322ba89fb4cfb7572e4b701d80782c
Mirko Mälicke <mirko@maelicke-online.de>
2020-03-31 19:49:38 +0200

    moving to conda env

--- lectures/configuration.js
+++ lectures/configuration.js
@@ -17,12 +17,26 @@ var Config ={
                 {
                     label: "Introduction",
                     link: "02_introduction.slides.html"
+                },
+                {
+                    label: "Univariate Statistics",
+                    link: "03_univariate_statistics.slides.html"
                 }
             ]
         },
         {
             label: "Notebooks",
-            navigation: []
+            navigation: [
+                {
+                    label: "Introduction",
+                    link: "02_introduction.html",
+                    binder: "https://mybinder.org/v2/gh/KIT-HYD/geostatistics_lecture/master?filepath=lectures%2F02_introduction.ipynb"
+                },
+                {
+                    label: "Univariate Statistics",
+                    link: "03_univariate_statistics.html"
+                }
+            ]
         },
         {
             label: "About",
